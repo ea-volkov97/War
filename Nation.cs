00f6220ead072967562d617b765639906d5199f8
evgenii_volkov <ea.volkov97@yandex.ru>
2020-09-20 18:11:21 +0300

    Added class Nation.

---
+++ Nation.cs
@@ -0,0 +1,29 @@
+﻿namespace War
+{
+    /// <summary>
+    /// Представляет собой игровую нацию, которая выступает в роли владельца юнитов.
+    /// </summary>
+    public class Nation
+    {
+        /// <summary>
+        /// Возвращает название нации.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Возвращает притяжательное прилагательное, образованное от названия нации. Используется в именах юнитов.
+        /// </summary>
+        public string WhoseUnitsText { get; private set; }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Nation"/> c указанным именем и притяжательным прилагательным.
+        /// </summary>
+        /// <param name="name">Название нации.</param>
+        /// <param name="whoseUnitsText">Притяжательное прилагательное, образованное от названия нации.</param>
+        public Nation(string name = "Unknown nation", string whoseUnitsText = "Unknown nation's")
+        {
+            Name = name;
+            WhoseUnitsText = whoseUnitsText;
+        }
+    }
+}
