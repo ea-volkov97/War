8cb57170ca954973ef8f24ca8939128596893459
evgenii_volkov <ea.volkov97@yandex.ru>
2020-09-21 13:38:48 +0300

    Added Unit abstract class and Squad class.

---
+++ Squad.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using War.Units;
+
+namespace War
+{
+    /// <summary>
+    /// Представляет описание отряда, состоящего из юнитов.
+    /// </summary>
+    public class Squad
+    {
+        /// <summary>
+        /// Возвращает или задает нацию, которой принадлежит данный отряд.
+        /// </summary>
+        public Nation Owner { get; private set; }
+        /// <summary>
+        /// Возвращает или задает юниты, которые входят в отряд.
+        /// </summary>
+        private List<Unit> _units;
+        /// <summary>
+        /// Возвращает количество юнитов в отряде.
+        /// </summary>
+        public int UnitsCount => _units == null ? 0 : _units.Count;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Squad"/>, принадлежащий указанной нации, из набора юнитов.
+        /// Если нация, которой принадлежит отряд, и нация, которой принадлежит юнит различны, то он не будет добавлен в отряд.
+        /// </summary>
+        /// <param name="nation"></param>
+        /// <param name="units">Список юнитов, из которых должен быть образован отряд.</param>
+        public Squad(Nation nation, List<Unit> units = null)
+        {
+            Owner = nation;
+
+            if (units != null)
+                foreach (Unit unit in units)
+                    TryAddUnit(unit);
+        }
+
+        /// <summary>
+        /// Возвращает True, если все юниты в отряде погибли. False - если есть хотя бы один выживший юнит.
+        /// </summary>
+        public bool isDefeated
+        {
+            get
+            {
+                foreach (Unit unit in _units)
+                    if (!unit.IsDead)
+                        return false;
+
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Производит расчет характеристик входящих в отряд юнитов с учетом бонусов перед атакой.
+        /// </summary>
+        public void PrepareToAttack()
+        {
+            foreach (Unit unit in _units)
+                unit.Influence();
+        }
+
+        /// <summary>
+        /// Атакует указанный отряд.
+        /// </summary>
+        /// <param name="enemySquad"></param>
+        public void Attack(Squad enemySquad)
+        {
+            foreach (Unit unit in _units)
+                for (int i = 0; i < enemySquad.UnitsCount; i++)
+                    unit.Attack(enemySquad.GetUnitByIndex(i));
+        }
+
+        /// <summary>
+        /// Возвращает юнит отряда по индексу.
+        /// </summary>
+        /// <param name="index">Индекс юнита.</param>
+        /// <returns>Юнит отряда.</returns>
+        public Unit GetUnitByIndex(int index)
+        {
+            if (index < 0 || index >= UnitsCount)
+                return _units[index];
+            else
+                return null;
+        }
+
+        /// <summary>
+        /// Присоединяет юнит к отряду, если нации, которым они принадлежат совпадают и возвращает true.
+        /// В противном случае возвращает false.
+        /// </summary>
+        /// <param name="unit">Присоединяемый юнит.</param>
+        /// <returns>True, если юнит был присоединен. False, если - нет.</returns>
+        public bool TryAddUnit(Unit unit)
+        {
+            if (unit.TryToJoinToSquad(this))
+            {
+                _units.Add(unit);
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ Units/Unit.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+namespace War.Units
+{
+    /// <summary>
+    /// Представляет абстрактное описание боевого юнита.
+    /// </summary>
+    public abstract class Unit
+    {
+        /// <summary>
+        /// Возвращает или задает имя юнита.
+        /// </summary>
+        public string Name { get; protected set; }
+        /// <summary>
+        /// Возвращает или задает количество очков здоровья юнита.
+        /// </summary>
+        public int Health { get; private set; }
+        /// <summary>
+        /// Возвращает или задает нацию, которая является владельцем юнита.
+        /// </summary>
+        public Nation Owner { get; private set; }
+        /// <summary>
+        /// Возвращает или задает текстовое описание юнита.
+        /// </summary>
+        public string Description { get; private set; }
+
+        /// <summary>
+        /// Возвращает true, если очки здоровья ниже или равно нулю.
+        /// </summary>
+        public bool IsDead => Health <= 0;
+        /// <summary>
+        /// Возвращает род войск юнита.
+        /// </summary>
+        public UnitType Type { get; protected set; }
+
+        /// <summary>
+        /// Возвращает ссылку на отряд, в котором состоит юнит.
+        /// </summary>
+        protected Squad _squad = null;
+
+        /// <summary>
+        /// Сила атаки юнита.
+        /// </summary>
+        private int _attackForce;
+        /// <summary>
+        /// Обороноспсобность юнита.
+        /// </summary>
+        private int _defendForce;
+        /// <summary>
+        /// Скорость восстановления здоровья юнита.
+        /// </summary>
+        private int _healSpeed;
+
+        /// <summary>
+        /// Коэффициент, определяющий бонус к силе атаке юнита.
+        /// </summary>
+        private float _attackForceBonus = 0f;
+        /// <summary>
+        /// Коэффициент, определяющий бонус к обороноспособность юнита.
+        /// </summary>
+        private float _defendForceBonus = 0f;
+        /// <summary>
+        /// Коэффициент, определяющий бонус к скорости восстановления здоровья юнита.
+        /// </summary>
+        private float _healBonus = 0f;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Unit"/> с указанными параметрами.
+        /// </summary>
+        /// <param name="owner">Нация, которой принадлежит данный юнит.</param>
+        /// <param name="name">Имя юнита.</param>
+        /// <param name="attackForce">Сила атаки юнита.</param>
+        /// <param name="defendForce">Обороноспособность юнита.</param>
+        /// <param name="healSpeed">Скорость восстановления здоровья юнита.</param>
+        /// <param name="description">Текстовое описание юнита.</param>
+        public Unit(Nation owner, string name = "", int attackForce = 0, int defendForce = 0, int healSpeed = 0, string description = "")
+        {
+            Owner = owner;
+            Name = name == "" ? "Unknown Unit" : name;
+
+            _attackForce = attackForce < 0 ? 0 : attackForce;
+            _defendForce = defendForce < 0 ? 0 : defendForce;
+            _healSpeed = healSpeed < 0 ? 0 : healSpeed;
+
+            Description = description;
+        }
+
+        /// <summary>
+        /// Добавляет юнит в указанный отряд, если это возможно и возвращает true. В противном  случае - возвращает false.
+        /// </summary>
+        /// <param name="squad">Отряд, к которому должен присоединиться юнит.</param>
+        /// <returns>Результат действия.</returns>
+        public bool TryToJoinToSquad(Squad squad)
+        {
+            if (squad.Owner == Owner)
+            {
+                _squad = squad;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Совершает атаку по указаннной цели.
+        /// <param name="enemyUnit">Цель.</param>
+        /// </summary>
+        public virtual void Attack(Unit enemyUnit)
+        {
+            int damage = Convert.ToInt32(_attackForce * (1 + _attackForceBonus));
+            enemyUnit.TakeDamage(damage);
+        }
+
+        /// <summary>
+        /// Совершает атаку по указанной группе целей.
+        /// </summary>
+        /// <param name="enemyUnits">Группа целей.</param>
+        public virtual void Attack(Unit[] enemyUnits)
+        {
+            int damage = Convert.ToInt32(_attackForce * (1 + _attackForceBonus));
+            foreach (Unit enemyUnit in enemyUnits)
+                enemyUnit.TakeDamage(damage);
+        }
+
+        /// <summary>
+        /// Принимает указанное число единиц урона.
+        /// </summary>
+        /// <param name="damage">Величина урона, которая будет нанесена юниту.</param>
+        public virtual void TakeDamage(int damage)
+        {
+            Health -= Convert.ToInt32(damage * (1 - _defendForce * (1 + _defendForceBonus)));
+        }
+
+        /// <summary>
+        /// Восстанавливает здоровье юнита.
+        /// </summary>
+        public virtual void Heal()
+        {
+            Health += Convert.ToInt32(_healSpeed * (1 + _healBonus));
+        }
+
+        /// <summary>
+        /// Задает величину бонусов к характеристикам юнита.
+        /// </summary>
+        /// <param name="attackForceBonus">Величина бонуса к атаке.</param>
+        /// <param name="defendForceBonus">Величина бонуса к обороноспособности.</param>
+        /// <param name="healBonus">Величина бонуса к скорости восстановления здоровья.</param>
+        public virtual void TakeInfluence(float attackForceBonus = 0, float defendForceBonus = 0, float healBonus = 0)
+        {
+            _attackForceBonus += attackForceBonus;
+            _defendForceBonus += defendForceBonus;
+            _healBonus += healBonus;
+        }
+
+        /// <summary>
+        /// Оказывает воздействие на других юнитов отряда, в котором состоит данный юнит.
+        /// </summary>
+        public abstract void Influence();
+
+        /// <summary>
+        /// Задает константы, которые определяют род войск юнита.
+        /// </summary>
+        public enum UnitType
+        {
+            LandForce,
+            AirForce
+        }
+    }
+}
